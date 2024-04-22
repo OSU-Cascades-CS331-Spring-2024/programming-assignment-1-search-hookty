d300a14766f203738902ec318a5aa9d55df051ba
Tristan <tristanskyhook@gmail.com>
2024-04-22 11:46:15 -0700

    gaming is the mind killer. gaming is the little swag that brings total ballin. i must not game

---
+++ dls.py
@@ -0,0 +1,19 @@
+import algo
+
+class DLS(algo.Algorithm):
+    def __init__(self, map, origin, destination):
+        frac = 3
+        self.max_depth = int(len(map.nodes) / frac)
+        self.current_depth = self.max_depth
+        super().__init__(map, origin, destination)
+
+    def chooseNode(self):
+        if self.current_depth > 0:
+            self.current_depth -= 1
+            return self.frontier.pop() # get the last element, the most recently added
+        else:
+            self.current_depth = self.max_depth
+            return self.frontier.pop(0) # max depth reached, try another path
+    
+    def search(self):
+        return super().search()
